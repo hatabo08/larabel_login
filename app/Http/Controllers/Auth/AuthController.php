255ddc312d7111b4821822ee97755c8ed40b34d7
hatabo <ss199618mr@gmail.com>
2025-03-30 19:22:01 +0900

    ログイン画面修正

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -31,5 +31,21 @@ class AuthController extends Controller
        ]);
     }
 
+/**
+ * ユーザーをアプリケーションからログアウトさせる
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @return \Illuminate\Http\Response
+ */
+public function logout(Request $request)
+{
+    Auth::logout();
+
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return redirect('/');
+}
     
 }
